6f52a06837d3052bbc4f525225a0235122cf4398
Enzo <enz.dard@gmail.com>
2022-07-26 14:34:34 +0200

    Create some Tests

---
+++ src/test/java/fr/minesales/middleware/teamb/FightTest.java
@@ -0,0 +1,7 @@
+package fr.minesales.middleware.teamb;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FightTest {
+}
---
+++ src/test/java/fr/minesales/middleware/teamb/SquadTest.java
@@ -0,0 +1,4 @@
+package fr.minesales.middleware.teamb;
+
+public class SquadTest {
+}
