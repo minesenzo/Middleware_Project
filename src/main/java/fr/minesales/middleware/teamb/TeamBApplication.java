22ca7818868568c65574a42df919395235aae97c
Enzo <enz.dard@gmail.com>
2022-07-26 12:35:08 +0200

    Initial commit

---
+++ src/main/java/fr/minesales/middleware/teamb/TeamBApplication.java
@@ -0,0 +1,13 @@
+package fr.minesales.middleware.teamb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/minesales/middleware/teamb/TeamBApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.minesales.middleware.teamb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
